cc2c754f2f1644f75c7ce36e98e165a8f67f515d
Ashu Endless <ashuendless28@gmail.com>
2021-12-14 22:44:06 +0530

    2nd

--- speak.js
+++ speak.js
@@ -1,51 +1,74 @@
 var synth = window.speechSynthesis;
-var voices  = synth.getVoices()
+var voices = synth.getVoices()
 console.log(voices)
-document.querySelector('button').onclick=()=>{
-    var utterThis = new SpeechSynthesisUtterance("hi it's me Endless");
-    //utterThis.voice = "Microsoft Ravi - English (India)"
-    utterThis.pitch = 1;
-    utterThis.rate = 1;
-    synth.speak(utterThis);
+document.querySelector('#start').onclick = () => {
+    Say("hi it's me Endless");
+    recognition.start();
 }
 
 
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral' ];
+var colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
+recognition.continuous = true;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+var listenCount = 0
 
-document.querySelectorAll('button')[1].onclick=()=>{
+function Say(str) {
+    var utterThis = new SpeechSynthesisUtterance(str);
+    //utterThis.voice = "Microsoft Ravi - English (India)"
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    synth.speak(utterThis);
+    //recognition.start()
+    console.log('jojoj')
+
+}
+
+document.querySelectorAll('button')[1].onclick = () => {
     recognition.start();
 }
 
-recognition.onresult = function(event) {
-    var color = event.results[0][0].transcript;
-    //diagnostic.textContent = 'Result received: ' + color + '.';
-    //bg.style.backgroundColor = color;
-    console.log('Confidence: ' + event.results[0][0].confidence);
-    console.log('transcript: ' + event.results[0][0].transcript);
-    if(event.results[0][0].transcript == "hello"){
-    var utterThis = new SpeechSynthesisUtterance("hello");
-    synth.speak(utterThis);}
-    else if(event.results[0][0].transcript == "hello"){
-        var utterThis = new SpeechSynthesisUtterance("hii");
-    synth.speak(utterThis);}
-    }
-  
+recognition.onresult = function (event) {
+    var recognised_words = event.results[listenCount][0].transcript.trim()
+    console.log(event);
+    console.log('Confidence: ' + event.results[listenCount][0].confidence);
+    console.log('transcript: ' + event.results[listenCount][0].transcript);
 
-recognition.onspeechend = function() {
+    listenCount += 1
+    if (recognised_words.includes("hello")) {
+        Say("hello");
+
+    }
+    else if (recognised_words == "hai") {
+        var utterThis = new SpeechSynthesisUtterance("hii");
+        synth.speak(utterThis);
+    }
+
+    else if (recognised_words.includes(" my pendding work today")) {
+        var utterThis = new SpeechSynthesisUtterance("you need to complete Interact work as soon as possible");
+        synth.speak(utterThis)
+    }
+
+    else if(recognised_words.includes("you are slow")){
+        Say('Ashu made me so better you ask him why i am slow')
+    }
+
+
+}
+
+
+recognition.onspeechend = function () {
     recognition.stop();
     console.log("end")
-  }
-  
+}
+
